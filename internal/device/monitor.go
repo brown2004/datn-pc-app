7681331579c7f7fa025fa2795d50ba6c9e3cee03
hhd <huyduong020304@gmail.com>
2026-04-03 18:01:17 +0700

    .

--- internal/device/monitor.go
+++ internal/device/monitor.go
@@ -1,3 +1,8 @@
 package device
 
+import "pc-app/internal/domain"
+
 // TODO: implement device monitor
+type Monitor struct {
+	out chan domain.DeviceEvent //out la mot cai channel chua ten cua event
+}
